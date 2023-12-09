8839f937eb2ea84b3908719cef41be19b565299c
Vlad Krupen <air7360@gmail.com>
2023-12-09 13:15:30 +0300

    Initial commit

---
+++ Homework_MovementSquare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_MovementSquare
+//
+//  Created by Vlad on 9.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
